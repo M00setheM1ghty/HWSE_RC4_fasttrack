80ef713f73f7b495492d8d9066f1c2411ad7c0e6
Thomas Baumann <ic25b121@technikum-wien.at>
2025-09-10 21:20:21 +0200

    - changed print array to hex format - corrected swap elements errors: key length, assignment of array [j] not j

--- main.c
+++ main.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 void swap_elements();
 void populate_array();
-void print_array();
+void print_array_hex();
 
 int main() {
-    char* test_key = "imatestkey";
+    char* test_key = "Key"; uint8_t key_length = strlen(test_key);
     uint8_t S[256];
     populate_array(S);
-    swap_elements(S,test_key);
-    print_array(S);
-    printf("%d\n", S[143]);
+    swap_elements(S,test_key,key_length);
+    print_array_hex(S);
+    //printf("%d\n", S[255]);
     return 0;
 }
 
 /**
-*@brief swap elements around according to provided pseudocode
+*@brief swap elements of array around according to provided pseudocode
 *@param array array with nums
 *@param key string with key
 *@return none
  **/
-void swap_elements(uint8_t* array, char* key) {
+void swap_elements(uint8_t* array, char* key, uint8_t key_length) {
     uint8_t j = 0;
-    for(uint8_t i = 0; i < 255; i++) {
-        j = (j + array[i] + key[i%(sizeof(key))]) % 256;
+    for(int i = 0; i < 256; i++) {
+        j = (j + array[i] + key[i%key_length]) % 256;
         uint8_t temp = array[i];
-        array[i] = j;
+        array[i] = array[j];
         array[j] = temp;
     }
 }
@@ -43,8 +44,8 @@ void populate_array(uint8_t* array) {
     }
 }
 
-void print_array(uint8_t* array) {
+void print_array_hex(uint8_t* array) {
     for(int i = 0; i < 256; i++) {
-        printf("%d\n", array[i]);
+        printf("%x\n", array[i]);
     }
 }
